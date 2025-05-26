0c6feaee098f7d70ba1bfaafd0a569435f598f2e
abi <abisheela88@gmail.com>
2025-05-26 21:52:40 +0530

    Initial commit

---
+++ FoodOrderApi/Migrations/20250523165637_foodorder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrderApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class foodorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.RenameTable(
+                name: "BillDetails",
+                newName: "BillDetails",
+                newSchema: "public");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "BillDetails",
+                schema: "public",
+                newName: "BillDetails");
+        }
+    }
+}
---
+++ FoodOrderApi/Migrations/20250526161105_InitialOrder.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodOrderApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
